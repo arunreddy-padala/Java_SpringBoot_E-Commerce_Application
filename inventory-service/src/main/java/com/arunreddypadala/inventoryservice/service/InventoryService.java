6adaff18def60da623857155c8da7a3809edb611
arunreddypadala <arunreddypadala@gmail.com>
2023-06-14 14:08:53 -0400

    Adding code for circuit breaker

--- inventory-service/src/main/java/com/arunreddypadala/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/arunreddypadala/inventoryservice/service/InventoryService.java
@@ -6,6 +6,9 @@ import com.arunreddypadala.inventoryservice.model.Inventory;
 import com.arunreddypadala.inventoryservice.repository.InventoryRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,11 +16,18 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
-    public List<InventoryResponse> isInStock(List<String> skuCode) {
+    @SneakyThrows
+    public List<InventoryResponse> isInStock(List<String> skuCode)  {
+        log.info("Wait Started");
+
+      Thread.sleep(10000);
+
+      log.info("Wait Ended");
 
          return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                  .map(inventory ->
--- order-service/src/main/java/com/arunreddypadala/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/arunreddypadala/orderservice/controller/OrderController.java
@@ -11,6 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.CompletableFuture;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -22,10 +27,17 @@ public class OrderController {
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
-  public String placeOrder(@RequestBody OrderRequest orderRequest) {
+  @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+  @TimeLimiter(name = "inventory")
+  @Retry(name = "inventory")
+  public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
+    return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
 
-    orderService.placeOrder(orderRequest);
-    return "Order Placed Successfully";
+  }
+
+  public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+
+  return CompletableFuture.supplyAsync(() -> "Oops! Something went wrong, please try again after sometime");
 
   }
 
